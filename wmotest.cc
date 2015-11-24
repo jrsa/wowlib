5e4c59f64b572b341c797c9c03a80caf9473142c
James Anderson <jrsa@jrsa.co>
2015-11-23 18:58:48 -0800

    add wmotest

--- wmotest.cc
+++ wmotest.cc
@@ -1,7 +1,14 @@
+#include <string>
 #include <glog/logging.h>
-#include "wmo/wmo.h"
+
+#include <wmo/wmo.h>
 
 int main(int argc, char const *argv[]) {
+
+  FLAGS_log_dir = "./wmotest_logs/";
+  google::InitGoogleLogging(argv[0]);
+
+
 
   // get the name of a wmo as input (w default for testing)
 

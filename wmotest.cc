3989dbc1636077c7f729086d926349645aaa8393
James Anderson <jrsa@jrsa.co>
2017-07-15 15:42:22 -0700

    almost an actual test case (hint hint)

--- wmotest.cc
+++ wmotest.cc
@@ -3,6 +3,7 @@
 #include <boost/filesystem.hpp>
 
 #include "wmo/wmo.hpp"
+#include "file/local_file.hpp"
 
 using namespace boost::filesystem;
 
@@ -16,10 +17,18 @@ int main(int argc, char const *argv[]) {
     wmopath = path(argv[1]);
   } else {
     wmopath =
-        path("../data/wowassets/wotlk/World/wmo/Azeroth/Buildings/NSabbey/");
+        path("/Users/jrsa/wow/wowassets/wotlk/World/wmo/Azeroth/Buildings/Stormwind/Stormwind.wmo");
+        // path("/Users/jrsa/wow/wowassets/wotlk/World/wmo/Dungeon/AZ_Karazahn/Kharazan.wmo");
   }
 
-  // get the name of a wmo as input (w default for testing)
+  local_file root_file( wmopath.string() );
+  wmo object;
+
+  object.load( root_file );
+
+  for( group_itr g = object.first_group(); g != object.last_group(); ++g ) {
+    LOG(INFO) << g->name() << " loaded";
+  }
 
   return 0;
 }

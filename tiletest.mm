a3da663a4ae9ffc6814a44601d865d731e6e3ec8
James Anderson <jrsa@jrsa.co>
2017-07-15 15:19:08 -0700

    something more like a real test case, if a test suite were ever adopted for this project

--- tiletest.mm
+++ tiletest.mm
@@ -1,4 +1,4 @@
-#include <Cocoa/Cocoa.h>
+//#include <Cocoa/Cocoa.h>
 
 #include <glog/logging.h>
 #include <string>
@@ -18,20 +18,21 @@ int main(int argc, char const *argv[]) {
   FLAGS_log_dir = ".";
   google::InitGoogleLogging(argv[0]);
 
-  NSOpenPanel* panel = [NSOpenPanel openPanel];
-
-  panel.message = @"select a map folder";
-  panel.canChooseDirectories = YES;
-  panel.canChooseFiles = NO;
-  [panel runModal];
-  
-  path mappath(panel.URLs[0].fileSystemRepresentation);
+//  NSOpenPanel* panel = [NSOpenPanel openPanel];
+//
+//  panel.message = @"select a map folder";
+//  panel.canChooseDirectories = YES;
+//  panel.canChooseFiles = NO;
+//  [panel runModal];
+//
+//  path mappath(panel.URLs[0].fileSystemRepresentation);
+  path mappath("/Users/jrsa/wow/wowassets/wotlk/World/Maps/Azeroth");
   path wdtpath(mappath);
   
-  std::string map_name(mappath.root_path().string());
+  std::string map_name(mappath.filename().string());
   
   std::string wdtname(map_name + ".wdt");
-  wdtpath /= map_name;
+  //wdtpath /= map_name;
   wdtpath /= wdtname;
 
   // load wdt lol
@@ -40,8 +41,8 @@ int main(int argc, char const *argv[]) {
   wowlib::wdt map(wdtfile);
 
 
-  int x_index(27);
-  int y_index(25);
+  int x_index(32);
+  int y_index(48);
 
   tile t1(map_name, x_index, y_index);
 
